97a1c469d100ce91edb96347df6bee5f6bffcd9b
Sergio Ferrando López <Sergio@iMac-de-Sergio.local>
2016-09-08 11:24:43 +0200

    empezando migracion de views de haml

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get '/' => 'home#index' #routa absoluta
+  get '/ticket/new' => 'tickets#new' #routa absoluta
+  get '/tickets' => 'tickets#show_all' #routa absoluta
+  get '/ticket/:id' => 'ticket#show' #routa absoluta
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
